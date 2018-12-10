263870cc682601bfbfda09135c037743b4161218
Jinny <44691775+ef10007@users.noreply.github.com>
2018-12-10 18:05:36 +0900

    Update DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -25,9 +25,9 @@ select * from Student;
 2. Prof table
 
 create table Prof(
- 	id smallint unsigned not null auto_increment primary key,
-  name varchar(31) not null,
-  likecnt int(0) default 0
+	id smallint unsigned not null auto_increment primary key,
+	name varchar(31) not null,
+ 	likecnt int(0) default 0
 );
 
 -- Creating Prof table
@@ -37,23 +37,35 @@ show table status ;
 show index from Prof;
 select * from Prof;
 
--- Check if Student Prof is correctly created
+-- Check if Prof is correctly created
 
 
 3. Subject table
 
 create table Subject(
 	id smallint unsigned not null auto_increment primary key,
-  name varchar(31) not null,
-  prof smallint unsigned,
-  constraint foreign key fk_prof(prof) references Prof(id) on delete set 
+	name varchar(31) not null,
+	prof smallint unsigned,
+	constraint foreign key fk_prof(prof) references Prof(id) on delete set 
  );
 
--- Creating Subject table
+-- Creating Subject table, prof column is a foreign key connected with id value in Prof table.
 
 desc Subject;
 show table status ;
 show index from Subject;
 select * from Subject;
 
--- Creating Subject table
+-- Check if Subject is correctly created
+
+4. Enrol table
+
+create table Enrol(
+	id int unsigned not null auto_increment primary key,
+	subject smallint unsigned not null,
+	student int not null,
+	constraint foreign key fk_subject(subject) references Subject(id) on delete cascade,
+	constraint foreign key fk_student(student) references Student(id) on delete cascade
+);
+
+-- Creating Enrol table, subject column is a foreign key connected with id value in Subject table on cascade key and student column is a foreign key connected with id value in Student table on cascade key.
