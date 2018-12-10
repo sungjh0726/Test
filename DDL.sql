9682020d9517ff9d4bd012963379f2e3b78ce335
Jinny <44691775+ef10007@users.noreply.github.com>
2018-12-10 17:51:35 +0900

    Update and rename DDL.md to DDL.sql

---
+++ DDL.sql
@@ -0,0 +1,19 @@
+
+CREATE TABLE `Student` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '학번',
+  `name` varchar(32) NOT NULL,
+  `addr` varchar(30) NOT NULL,
+  `birth` varchar(8) NOT NULL,
+  `tel` varchar(15) NOT NULL,
+  `email` varchar(31) NOT NULL,
+  `regdt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `index_Students_tel` (`tel`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+
+select * from Student;
+desc Student;
+
+show table status ;
+show index from Student;
