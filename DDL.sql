36e9fec8a333fc22fedc0097fe9cb5af8fd1825a
Jinny <44691775+ef10007@users.noreply.github.com>
2018-12-10 18:08:02 +0900

    Update DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -11,14 +11,12 @@ CREATE TABLE `Student` (
   PRIMARY KEY (`id`),
   KEY `index_Students_tel` (`tel`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
-
 -- Creating Student table
 
 desc Student;
 show table status ;
 show index from Student;
 select * from Student;
-
 -- Check if Student table is correctly created
 
 
@@ -29,14 +27,12 @@ create table Prof(
 	name varchar(31) not null,
  	likecnt int(0) default 0
 );
-
 -- Creating Prof table
 
 desc Prof;
 show table status ;
 show index from Prof;
 select * from Prof;
-
 -- Check if Prof is correctly created
 
 
@@ -48,14 +44,12 @@ create table Subject(
 	prof smallint unsigned,
 	constraint foreign key fk_prof(prof) references Prof(id) on delete set 
  );
-
 -- Creating Subject table, prof column is a foreign key connected with id value in Prof table.
 
 desc Subject;
 show table status ;
 show index from Subject;
 select * from Subject;
-
 -- Check if Subject is correctly created
 
 4. Enrol table
@@ -67,7 +61,6 @@ create table Enrol(
 	constraint foreign key fk_subject(subject) references Subject(id) on delete cascade,
 	constraint foreign key fk_student(student) references Student(id) on delete cascade
 );
-
 -- Creating Enrol table, subject column is a foreign key connected with id value in Subject table on cascade key and
 -- student column is a foreign key connected with id value in Student table on cascade key.
 
@@ -75,6 +68,5 @@ desc Enrol;
 show table status ;
 show index from Enrol;
 select * from Enrol;
-
 -- Check if Subject is correctly created
 
