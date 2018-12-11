a4af781528d047aa7045ce2e3a760a71ce4e5928
Jinny <44691775+ef10007@users.noreply.github.com>
2018-12-11 12:06:08 +0900

    Update ClubMember_table.sql

--- ClubMember_table.sql
+++ ClubMember_table.sql
@@ -1,11 +1,36 @@
 
--- creating ClubMember table
-
 CREATE TABLE ClubMember (
-  id smallint unsigned NOT NULL auto_increment primary key,
+	id smallint unsigned NOT NULL auto_increment primary key,
 	club smallint unsigned NOT NULL,
 	student int(11) not null,
-  level smallint not null default 0,
+	level smallint not null default 0,
 	constraint foreign key fk_club(club) references Club(id),
-  constraint foreign key fk_student(student) references Student(id)
+	constraint foreign key fk_student(student) references Student(id)
 );
+
+desc ClubMember;
+show table status;
+show index from ClubMember;
+-- creating ClubMember table
+
+
+select * from ClubMember;
+
+insert into ClubMember(club, student) select 1, id from Student order by rand() limit 50;
+insert into ClubMember(club, student) select 2, id from Student order by rand() limit 50;
+insert into ClubMember(club, student) select 3, id from Student order by rand() limit 50;
+insert into ClubMember(club, student) select 4, id from Student order by rand() limit 50;
+select * from ClubMember;
+-- inserting values Club 1,2,3,4 
+
+select club, count(*) from ClubMember group by club;
+-- reading the number of student grouped by club 1,2,3,4 
+ 
+ 
+ 
+update ClubMember set level = rand() where id > 0; 
+update ClubMember set level = 2 where student = 100; 
+select * from ClubMember;
+-- giving level value 2 to indicate leader
+
+
