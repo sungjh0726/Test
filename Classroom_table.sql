d750a52403487c7c374e003c0be2ccbd6846a08d
Jinny <44691775+ef10007@users.noreply.github.com>
2018-12-11 13:02:39 +0900

    Update Classroom_table.sql

--- Classroom_table.sql
+++ Classroom_table.sql
@@ -1,25 +1,25 @@
 
 create table Classroom(
-	id smallint unsigned not null auto_increment primary key
+	id smallint unsigned not null auto_increment primary key,
+	name varchar(31) not null
 );
 
 select * from Classroom;
 -- creating Classroom table
 
-insert into Classroom(id) value('101');
-insert into Classroom(id) value('102');
-insert into Classroom(id) value('103');
-insert into Classroom(id) value('104');
-insert into Classroom(id) value('105');
-insert into Classroom(id) value('106');
-insert into Classroom(id) value('107');
-insert into Classroom(id) value('108');
-insert into Classroom(id) value('109');
-insert into Classroom(id) value('110');
-select count(*) from Classroom;
--- inserting values from 101 to 110 and count to check the number of each classroom
 
-select id from Classroom order by rand() limit 1;
-update Subject set classroom = (select id from Classroom order by rand() limit 1);
+insert into Classroom(name)
+select concat((id +100), '호') from Subject;
+-- inserting values Subject table's id + 100 by using concat()
 
+
+alter table Subject add column classroom smallint unsigned;
+alter table Subject add foreign key fk_classroom(classroom) references Classroom(id);
+-- altering Subject table, adding column classroom and foreign key with Classroom's id
+
+
+update Subject set classroom = (select name from Classroom order by rand() limit 1);
 select * from Subject;
+-- allocating name to classroom column in Subject table
+
+
