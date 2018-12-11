c23813e0d2c68bf925dbebc05372e427b5b7a138
Jinny <44691775+ef10007@users.noreply.github.com>
2018-12-11 13:07:13 +0900

    Update DML.sql

--- DML.sql
+++ DML.sql
@@ -5,3 +5,12 @@ create table Enrol(
     constraint foreign key fk_subject(subject) references Subject(id) on delete cascade,
     constraint foreign key fk_student(student) references Student(id) on delete cascade
 );
+
+desc Enrol;
+select * from Enrol;
+-- creating Enrol table
+
+
+insert into Enrol(subject, student) select 1, sd.id from Student sd order by rand() on duplicate key update student=student;
+update Enrol set student = (select sd.id from Student sd order by rand() limit 1) where id !=0;
+select count(distinct student) from Enrol;
