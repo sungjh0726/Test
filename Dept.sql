de0eb387f02a0cd2641873971bc155d9084caf01
Jinny <44691775+ef10007@users.noreply.github.com>
2018-12-11 12:29:35 +0900

    Update Dept.sql

--- Dept.sql
+++ Dept.sql
@@ -12,4 +12,17 @@ show index from Dept;
 select * from Dept;
 -- creating Dept table 
 
+update Dept d inner join Student sd on d.student = sd.id set d.student = '과대표';
+--  student column in Dept table is connected with Student table id
 
+insert into Dept(name, prof, student) 
+select '사회', 
+(select pf.id from Prof pf order by rand() limit 1), 
+(select sd.id from Student sd order by rand() limit 1);
+-- repeat the query above 4 more times (the name values varies ex. '화학', '생물', '도덕', '영어')
+
+alter table Student add column dept smallint unsigned;
+alter table Student add constraint foreign key fk_dept(dept) references Dept(id);
+update Student set dept = (select id from Dept order by rand() limit 1);
+select * from Student;
+-- altering Student table and updating dept column values randomly from Dept id
