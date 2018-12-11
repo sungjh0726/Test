da0065e4f4648f43667a4d780b540c47358dfbac
Jinny <44691775+ef10007@users.noreply.github.com>
2018-12-11 12:10:16 +0900

    Update Dept.sql

--- Dept.sql
+++ Dept.sql
@@ -1,7 +1,15 @@
 CREATE TABLE Dept(
-  id smallint unsigned NOT NULL auto_increment primary key,
+	id smallint unsigned NOT NULL auto_increment primary key,
 	name varchar(45) not null,
 	prof smallint unsigned not null,
-  student smallint unsigned not null default 0,
-  constraint foreign key fk_prof(prof) references Prof(id)
+	student smallint unsigned not null default 0,
+	constraint foreign key fk_prof(prof) references Prof(id)
 );
+
+desc Dept;
+show table status;
+show index from Dept;
+select * from Dept;
+-- creating Dept table 
+
+
