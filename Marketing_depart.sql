44cb308d6805aa6c44cc6a21a7e2dc7ff8c8ea61
Jinny <44691775+ef10007@users.noreply.github.com>
2018-12-18 14:30:41 +0900

    Update Marketing_depart.sql

--- Marketing_depart.sql
+++ Marketing_depart.sql
@@ -1,5 +1,4 @@
-
 select e.last_name, e.salary, d.department_name
-from employees e inner join departments d on e.department_id= d.department_id
-where department_name =  'Marketing'
+  from employees e inner join departments d on e.department_id= d.department_id
+  where department_name =  'Marketing'
 and e.salary < (select round(avg(salary)) from employees where department_id= 80);
