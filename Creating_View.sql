f1cb5d45b0ade9bb024a1c91033eb40f2ab9db14
Jinny <44691775+ef10007@users.noreply.github.com>
2018-12-18 14:23:33 +0900

    Update and rename Creating View v_grade_subject_average.sql to Creating_View.sql

--- Creating_View.sql
+++ Creating_View.sql
@@ -3,9 +3,8 @@ drop view if exists v_grade_subject_average;
 create view v_grade_subject_average as
 select sub.student_id, sub.student_name, sub.subjects_count, ((g.midterm + g.finalterm) /2) as 'average'
  from Grade g inner join (
-select e.id, stu.id as 'student_id', stu.name as 'student_name', stu.subjects as 'subjects_count' from Enrol e
-inner join Student stu on e.student = stu.id) sub
-on g.enroll = sub.id;
-
+     select e.id, stu.id as 'student_id', stu.name as 'student_name', stu.subjects as 'subjects_count' 
+     from Enrol e inner join Student stu on e.student = stu.id) sub
+ on g.enroll = sub.id;
 
 select * from v_grade_subject_average;
