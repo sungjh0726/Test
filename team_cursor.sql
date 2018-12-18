f5f2b77f054b8db235bd2e9bd6dae089e3277a2c
Jinny <44691775+ef10007@users.noreply.github.com>
2018-12-18 14:35:37 +0900

    Update team_cursor.sql

--- team_cursor.sql
+++ team_cursor.sql
@@ -18,54 +18,54 @@ begin
 
 	drop table if exists T_table0;
         create temporary table T_table0(
-	       subject varchar(31),
-	       student varchar(45),
-	       avr varchar(45)
-	       );
+	subject varchar(31),
+	student varchar(45),
+	avr varchar(45)
+	);
 
 	 drop table if exists T_table1;
          create temporary table T_table1(
-	        subject varchar(31),
-                student1 varchar(31),
-                score1 varchar(31),
-                student2 varchar(31),
-                score2 varchar(31),
-                student3 varchar(31),
-                score3 varchar(31)
-                );
+	 subject varchar(31),
+         student1 varchar(31),
+         score1 varchar(31),
+         student2 varchar(31),
+         score2 varchar(31),
+         student3 varchar(31),
+         score3 varchar(31)
+         );
             
 
 	while (local_i <= 16) do
-			insert into T_table0(subject, student, avr)
-            select max(sub.subject), group_concat(sub.student) as student, group_concat(sub.avr) as avr
-            from (select sbj.name as subject, stu.name as student, vge.avr as avr
-                  from v_grade_enrol as vge inner join Subject as sbj on sbj.id = vge.subject
-                                             inner join Student as stu on stu.id = vge.student
-                  where vge.subject = local_i order by avr desc limit 3) sub;
+	insert into T_table0(subject, student, avr)
+        select max(sub.subject), group_concat(sub.student) as student, group_concat(sub.avr) as avr
+        from (select sbj.name as subject, stu.name as student, vge.avr as avr
+              from v_grade_enrol as vge inner join Subject as sbj on sbj.id = vge.subject
+                                        inner join Student as stu on stu.id = vge.student
+              where vge.subject = local_i order by avr desc limit 3) sub;
 				
-			set local_i = local_i + 1;
-		end while;            
+	set local_i = local_i + 1;
+	end while;            
             
-		select * from T_table0;
+	select * from T_table0;
         
         open cursor_1;
             
-			loop1 : loop
+	loop1 : loop
                 
-				fetch cursor_1 into _subject, _student, _avr;
+	fetch cursor_1 into _subject, _student, _avr;
                 
-                if _isdone then
-					leave loop1;
-				end if;
+        if _isdone then
+	leave loop1;
+	end if;
                 
-				insert into T_table1 value(_subject, substring_index(_student, ',', 1), substring_index(_avr, ',', 1),
-                                                     substring_index(substring_index(_student, ',', 2),',',-1),
-                                                     substring_index(substring_index(_avr, ',', 2),',',-1),
-                                                     substring_index(substring_index(_student, ',', 3),',',-1),
-                                                     substring_index(substring_index(_avr, ',', 3),',',-1)); 
+	insert into T_table1 value(_subject, substring_index(_student, ',', 1), substring_index(_avr, ',', 1),
+                                   substring_index(substring_index(_student, ',', 2),',',-1),
+                                   substring_index(substring_index(_avr, ',', 2),',',-1),
+                                   substring_index(substring_index(_student, ',', 3),',',-1),
+                                   substring_index(substring_index(_avr, ',', 3),',',-1)); 
 				
                 
-            end loop loop1;
+        end loop loop1;
             
         close cursor_1;
         
