ccd0107253ec4909f4fcfe7e097e494863437bda
Jinny <44691775+ef10007@users.noreply.github.com>
2018-12-18 14:29:44 +0900

    Create Marketing_depart.sql

---
+++ Marketing_depart.sql
@@ -0,0 +1,5 @@
+
+select e.last_name, e.salary, d.department_name
+from employees e inner join departments d on e.department_id= d.department_id
+where department_name =  'Marketing'
+and e.salary < (select round(avg(salary)) from employees where department_id= 80);
